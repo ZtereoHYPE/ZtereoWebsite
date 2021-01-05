7289678940008334eeef613fe83a66ad33cc3027
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2021-01-06 00:12:50 +0100

    progress...? maybe

---
+++ scripts/arrows.js
@@ -0,0 +1,21 @@
+document.getElementsByClassName('') window.onscroll()
+
+
+
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+
+
+    // Only completely visible elements return true:
+    // THis is important ^^
+
+
+
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}
