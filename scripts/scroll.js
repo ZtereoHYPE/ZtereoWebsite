3f7731d533ab0f3569ddcf03235a5ec60483bf46
ZtereoHYPE <lorenzodegrandis@icloud.com>
2021-02-28 03:04:01 +0100

    new: projects page done

---
+++ scripts/scroll.js
@@ -0,0 +1,6 @@
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 230) {
+        document.getElementById("firstTitle").innerHTML = "Website";
+        document.getElementById("secondTitle").innerHTML = "to my welcome";
+      }
+};
